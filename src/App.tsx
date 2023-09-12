4e0833939c34c892e7b21c4abe5370e518e364e6
Geovane Barbosa <gbarbosa@topconsuite.com>
2023-09-12 19:55:23 -0300

    Creating application text areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Github } from 'lucide-react';
 import { Button } from "./components/ui/button";
 import { Separator } from './components/ui/separator';
+import { Textarea } from './components/ui/textarea';
 
 export function App() {
   return (
@@ -23,7 +24,26 @@ export function App() {
       </div>
 
       <main className='flex-1 p-6 flex gap-6'>
-        hi
+        <div className='flex flex-col flex-1 gap-4'>
+          <div className='grid grid-rows-2 gap-4 flex-1'>
+            <Textarea
+              className='resize-none p-4 leading-relaxed'
+              placeholder='Inclua o prompt para a IA...'
+            />
+            <Textarea
+              className='resize-none p-4 leading-relaxed'
+              placeholder='Resultado gerado pela IA...'
+              readOnly
+            />
+          </div>
+
+          <p className= 'text-sm text-muted-foreground'>
+            Lembre-se: você pode utilizar a variável <code className='text-violet-400'>{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+          </p>
+        </div>
+        <aside className='w-80'>
+          
+        </aside>
       </main>
     </div>
   )
